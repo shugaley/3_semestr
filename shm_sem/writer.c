1227afc771695124960a6d5933368c1dca34fa8e
shugaley <shugalei.niu@phystech.edu>
2020-10-14 15:41:05 +0300

    Add shared memory

--- shm_sem/writer.c
+++ shm_sem/writer.c
@@ -1,11 +1,11 @@
 
 #include "shm_sem.h"
 
-#include "assert.h"
-#include "stdio.h"
-#include "stdlib.h"
-#include "sys/ipc.h"
-#include "sys/shm.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 #include "shm_sem_general.h"
 
