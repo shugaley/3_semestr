71ad5b524e25f5583e8c4303bec6494a97ae6418
shugaley <shugalei.niu@phystech.edu>
2020-10-14 15:46:05 +0300

    Test commit

--- shm_sem/writer.c
+++ shm_sem/writer.c
@@ -20,7 +20,6 @@ void WriteSharedMemory(const char* pathInput)
     char* shared_memory = GetSharedMemory(FTOK_PATHNAME, FTOK_PROJ_ID, &shmid);
 
 
-
     int ret_shmctl = shmctl(shmid, IPC_RMID, NULL);
     if (ret_shmctl < 0) {
         perror("Error shmctl()");
