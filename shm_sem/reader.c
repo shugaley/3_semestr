964bcda28dd4254ef16f6f1b52ebeb49b4cd325d
shugaley <shugalei.niu@phystech.edu>
2020-10-20 02:38:56 +0300

    Add ReadData()

--- shm_sem/reader.c
+++ shm_sem/reader.c
@@ -1,18 +1,20 @@
 
 #include "shm_sem.h"
 
+#include <assert.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
 #include "shm_sem_general.h"
 
 //Debug
 #include "unistd.h"
+
+void ReadData(const char* shmaddr, int semid);
+
 
 void ReadSharedMemory ()
 {
@@ -23,6 +25,7 @@ void ReadSharedMemory ()
     char* shmaddr = ConstructSharedMemory(FTOK_PATHNAME, FTOK_PROJ_ID,
                                                 SIZE_SHARED_MEMORY, &shmid);
 
+    ReadData(shmaddr, semid);
     DestructSharedMemory(shmaddr, shmid);
 
     errno = 0;
@@ -30,5 +33,24 @@ void ReadSharedMemory ()
     if (ret_semctl < 0) {
         perror("Error ret_semctl()");
         exit(EXIT_FAILURE);
+    }
+}
+
+
+void ReadData(const char* shmaddr, int semid)
+{
+    assert(shmaddr);
+
+    while(1) {
+        Semop(semid, NUM_SEMAPHORES_IS_FULL, -1, 0);
+        Semop(semid, NUM_SEMAPHORES_MUTEX,   -1, 0);
+
+        if(*shmaddr == '\0')
+            break;
+        printf("%s", shmaddr);
+        fflush(stdout);
+
+        Semop(semid, NUM_SEMAPHORES_MUTEX,    1, 0);
+        Semop(semid, NUM_SEMAPHORES_IS_EMPTY, 1, 0);
     }
 }
