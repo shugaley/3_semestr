a9a5e22ff907b27d858d6a871376462810417818
shugaley <shugalei.niu@phystech.edu>
2020-10-19 12:40:28 +0300

    Add semaphores

--- shm_sem/shm_sem.h
+++ shm_sem/shm_sem.h
@@ -7,12 +7,14 @@ static const char FTOK_PATHNAME[] = __FILE__;
 static const int  FTOK_PROJ_ID    = 0;
 
 static const size_t SIZE_SHARED_MEMORY = 4096;
-static const size_t N_SEMAPHORES       = 3;
+static const size_t N_SEMAPHORES       = 5;
 
 enum NumSemaphores {
     NUM_SEMAPHORES_EXIST_WRITER = 0,
     NUM_SEMAPHORES_EXIST_READER = 1,
     NUM_SEMAPHORES_MUTEX        = 2,
+    NUM_SEMAPHORES_IS_EMPTY     = 3,
+    NUM_SEMAPHORES_IS_FULL      = 4,
 };
 
 void WriteSharedMemory(const char* path_input);
