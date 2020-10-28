7feef569b0db3ef642e5eb0efb78c9e8aebe9045
shugaley <shugalei.niu@phystech.edu>
2020-10-28 20:03:07 +0300

    Work base version of signals

--- signals/signals.c
+++ signals/signals.c
@@ -1,5 +1,5 @@
 
-#include "signal.h"
+#include "signals.h"
 
 #include <assert.h>
 #include <errno.h>
@@ -9,15 +9,27 @@
 #include <sys/prctl.h>
 #include <unistd.h>
 
+#define SIGNALS_USED 1
+
 void SendData(const char* path_input);
 void GetData (pid_t pid_child);
 
-void HandleOne(int num_signal, siginfo_t* siginfo, void* context);
+void HandleBitZero(int num_signal);
+void HandleBitOne (int num_signal);
 
+void BlockSignals(int flag, const int* signals, size_t nsignals);
 
-void TransferDataToChild(const char* path_input)
+//=============================================================================
+
+void TransferDataFromChild(const char* path_input)
 {
     assert(path_input);
+
+    //block signals which use
+
+    int signals_blocking[SIGNALS_USED] = {SIGUSR1};
+
+    BlockSignals(SIG_BLOCK, signals_blocking, SIGNALS_USED);
 
     pid_t pid_parent = getpid();
     pid_t pid_child = 0;
@@ -53,15 +65,89 @@ void TransferDataToChild(const char* path_input)
 void SendData(const char* path_input)
 {
     assert(path_input);
+
+    sleep(5);
+
+    pid_t pid_parent = getppid();
+    printf("Child kill\n");
+    kill(pid_parent, SIGUSR1);
 }
 
 
 void GetData(pid_t pid_child)
 {
+    struct sigaction sa_one = {};
+    errno = 0;
+    int ret = sigfillset(&sa_one.sa_mask);
+    if (ret < 0) {
+        perror("Error sigfillset()");
+        exit(EXIT_FAILURE);
+    }
+    sa_one.sa_handler = HandleBitOne;
+    sa_one.sa_flags = SA_NODEFER;
+
+    errno = 0;
+    ret = sigaction(SIGUSR1, &sa_one, NULL);
+    if (ret < 0) {
+        perror("Error sigaction()");
+        exit(EXIT_FAILURE);
+    }
+
+    sigset_t sigset_parent;
+    errno = 0;
+    ret = sigfillset(&sigset_parent);
+    if (ret < 0) {
+        perror("Error sigfillset()");
+        exit(EXIT_FAILURE);
+    }
+
+    errno = 0;
+    ret = sigdelset(&sigset_parent, SIGUSR1);
+    if (ret < 0) {
+        perror("Error sigdelset()");
+        exit(EXIT_FAILURE);
+    }
+
+    sigsuspend(&sigset_parent);
+
+    printf("Parent ready\n");
+
+    while(1);
 }
 
 
-void HandleOne(int num_signal, siginfo_t* siginfo, void* context)
+void HandleBitOne(int num_signal)
 {
-    *((int*)context) = 1;
+    printf("SSSSSSSUCK\n");
+}
+
+
+
+void BlockSignals(int flag, const int* signals, size_t nsignals)
+{
+    assert(signals);
+
+    sigset_t sigset_base = {};
+    errno = 0;
+    int ret = sigemptyset(&sigset_base);
+    if (ret < 0) {
+        perror("Error sigemptyset()");
+        exit(EXIT_FAILURE);
+    }
+
+    for (size_t i_signal = 0; i_signal < nsignals; i_signal++) {
+        ret = sigaddset(&sigset_base, signals[i_signal]);
+        if (ret < 0) {
+            perror("Error sigaddset()");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    errno = 0;
+    ret = sigprocmask(SIG_BLOCK, &sigset_base, NULL);
+    if (ret < 0) {
+        perror("Error sigprocmask()");
+        exit(EXIT_FAILURE);
+    }
+
 }
