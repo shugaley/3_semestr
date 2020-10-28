eaffe84862e0f872c030ac3dccc46f8d5513149d
shugaley <shugalei.niu@phystech.edu>
2020-10-28 20:03:46 +0300

    Small cosmetic fix

--- signals/signals.h
+++ signals/signals.h
@@ -1,6 +1,6 @@
 #ifndef SIGNALS_SIGNALS_H
 #define SIGNALS_SIGNALS_H
 
-void TransferDataToChild(const char* path_input);
+void TransferDataFromChild(const char* path_input);
 
 #endif //SIGNALS_SIGNALS_H
