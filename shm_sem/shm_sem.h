52d1326b705e195a8208ca93e6bf6fc0527bb839
shugaley <shugalei.niu@phystech.edu>
2020-11-24 14:39:51 +0300

    Change the set of semaphores

--- shm_sem/shm_sem.h
+++ shm_sem/shm_sem.h
@@ -9,22 +9,15 @@ static const char FTOK_PATHNAME[] = __FILE__;
 static const int  FTOK_PROJ_ID    = 0;
 
 static const size_t SIZE_SHARED_MEMORY = 4096;
-#define N_SEMAPHORES 5
+static const size_t N_SEMAPHORES = 5;
 
 enum NumSemaphores {
-    NUM_SEMAPHORES_IS_FREE_WRITER = 0,
-    NUM_SEMAPHORES_IS_FREE_READER = 1,
-    NUM_SEMAPHORES_MUTEX          = 2,
-    NUM_SEMAPHORES_IS_EMPTY       = 3,
-    NUM_SEMAPHORES_IS_FULL        = 4,
+    SEM_READER    = 0,
+    SEM_WRITER    = 1,
+    SEM_READ_SHM  = 2,
+    SEM_WRITE_SHM = 3,
+    SEM_N_ACTIVE  = 4,
 };
-
-static const struct SemaphoreData SEM_INIT_DATA[N_SEMAPHORES] =
-        {NUM_SEMAPHORES_IS_FREE_WRITER, 1,
-         NUM_SEMAPHORES_IS_FREE_READER, 1,
-         NUM_SEMAPHORES_MUTEX,          1,
-         NUM_SEMAPHORES_IS_EMPTY,       1,
-         NUM_SEMAPHORES_IS_FULL,        0};
 
 
 void WriteSharedMemory(const char* path_input);
